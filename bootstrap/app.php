599e5768e291c40c78f2573670cfabf582362f43
juanc <juancruzliberati@hotmail.com>
2026-01-11 02:29:27 -0300

    add group and alias middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\BFF\Mobile\V1\Middlewares\ApiResponseMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // creo mi propia colleccion de middlewares
+        $middleware->group('bbf.mobile.v1.api',
+            [
+                SubstituteBindings::class,
+                ApiResponseMiddleware::class,
+            ]);
+        //alias del middleware para la autenticacion del token
+        $middleware->alias([
+            'bff.mobile.v1.auth' => \App\BFF\Mobile\V1\Middlewares\EnsureIsAuthenticated::class,
+        ]);
+        ///dd($middleware->getMiddlewareGroups());
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
